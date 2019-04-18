02b5ff5db95fdc95685a4ccd66241892d85b37f7
Ricky Valdez <vldzrcky@gmail.com>
2019-04-18 09:49:42 -0400

    Comments added

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -1,13 +1,17 @@
-import java.util.*;
+import java.util.Map;
 
 public class GeneticAlgorithm {
-	
-	private Map<Double[],Double> population;
-	private int chromosomeLength;
-	private int populationSize;
+
+	///// ATTRIBUTES /////
+
+    private Map<Double[],Double> population;
+    private int chromosomeLength;
+    private int populationSize;
     private double percentChild = 0.6;
     private double percentMutate = 0.1;
     
+    ///// CONSTRUCTOR /////
+
     public GeneticAlgorithm(int popSize, int chrmLen)
     {
     	// TODO: Complete this constructor
@@ -16,21 +20,40 @@ public class GeneticAlgorithm {
     	chromosomeLength = chrmLen;
     }
 
+    ///// METHODS /////
+
+    /** 
+    * initializes the private variable population with random values
+    */
     public void initializePopulation()
     {
     	// TODO: Write this method
     }
 
-    public void nextGeneration()
+    /**
+    * nextGeneration method updates the population table by a single generation 
+    */
+    private void nextGeneration()
     {
     	// TODO: Write this method
     }
 
+    /*
+    * train method performs a number of generations specified by parameter 
+    * maxGenerations
+    */
     public void train(int maxGenerations)
     {
     	// TODO: Write this method
     }
 
+    /*
+    * crossOver method performs cross over on parameters parent1 and parent2 and 
+    * producing a single child chromosome.  There is a random chance that mutation
+    * will occur in the child specified by attribute percentMutate.  If the child
+    * by crossOver already exists in the population, perform crossover again,
+    * else return the child chromosome.
+    */
     private double[] crossOver(double[] parent1, double[] parent2)
     {
     	// TODO: Write this method
@@ -38,6 +61,10 @@ public class GeneticAlgorithm {
     	return new double[0];
     }
 
+    /**
+    * mutate randomly chooses a gene in chromosome and chooses a new random value
+    * for that gene.  The resulting chromosome is returned.
+    */
     private double[] mutate(double[] chromosome)
     {
     	// TODO: Write this method
