3187c94c7812bb5cd35eaf133b09c05be8068a32
emanh13 <emmanuel.hernandez33@gmail.com>
2019-06-05 17:28:05 -0400

    mutate method fixed

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -71,7 +71,7 @@ public class GeneticAlgorithm {
     * mutate randomly chooses a gene in chromosome and chooses a new random value
     * for that gene.  The resulting chromosome is returned.
     */
-    private double[] mutate(double[] chromosome)
+    public double[] mutate(double[] chromosome)
     {
     	// TODO: Write this method
     	Random rand = new Random();
