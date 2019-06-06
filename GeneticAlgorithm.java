fa04e3faea828a945fc6cbcd8eae9793d1e990dd
emanh13 <emmanuel.hernandez33@gmail.com>
2019-06-06 17:25:02 -0400

    Completed crossOver method.

--- GeneticAlgorithm.java
+++ GeneticAlgorithm.java
@@ -1,6 +1,6 @@
 import java.util.ArrayList;
 import java.util.Random;
-
+import java.lang.Math;
 
 public class GeneticAlgorithm {
 
@@ -34,6 +34,7 @@ public class GeneticAlgorithm {
     public void initializePopulation()
     {
     	// TODO: Write this method
+    	
     }
 
     /**
@@ -44,27 +45,65 @@ public class GeneticAlgorithm {
     	// TODO: Write this method
     }
 
-    /*
+    /**
     * train method performs a number of generations specified by parameter 
     * maxGenerations
     */
     public void train(int maxGenerations)
     {
     	// TODO: Write this method
+    	
     }
 
-    /*
+    /**
     * crossOver method performs cross over on parameters parent1 and parent2 and 
-    * producing a single child chromosome.  There is a random chance that mutation
-    * will occur in the child specified by attribute percentMutate.  If the child
-    * by crossOver already exists in the population, perform crossover again,
-    * else return the child chromosome.
+    * producing a two child chromosomes.  There is a random chance that mutation
+    * will occur in the children specified by attribute percentMutate. Returns 
+    * two children.
     */
-    private double[] crossOver(double[] parent1, double[] parent2)
+    public double[][] crossOver(double[] parent1, double[] parent2)
     {
-    	// TODO: Write this method
+        Random rand = new Random();
+        
+        int crossOverPoint;   //point where child will split parent 1 and parent 2
+        double[][] children = new double [2][chromosomeLength]; // two dimentional array for two children
+    	if (chromosomeLength >2)
+    	{
+            crossOverPoint = rand.nextInt(chromosomeLength-2)+1;
+        }
+        else
+        {
+            crossOverPoint = 1;
+        }
+    	
+    	for (int i=0; i < chromosomeLength; i++)
+    	{
+            if (i < crossOverPoint)          // if coin lands on 0, choose parent 1
+            {
+                children[0][i] = parent1[i];
+                children[1][i] = parent2[i];
+            }
+            else if (i >= crossOverPoint)     // if coin lands on 1, choose parent 2
+            {
+                children[0][i] = parent2[i];
+                children[1][i] = parent1[i];
+            }
+            else{}
+        }
+        
+        
+        if (rand.nextDouble() <= percentMutate)
+        {
+            mutate(children[0]);
+        }
+        else{}
+        if (rand.nextDouble() <= percentMutate)
+        {
+            mutate(children[1]);
+        }
+        else{}
 
-    	return new double[0];
+    	return children;
     }
 
     /**
@@ -73,7 +112,6 @@ public class GeneticAlgorithm {
     */
     private double[] mutate(double[] chromosome)
     {
-    	// TODO: Write this method
     	Random rand = new Random();
     	int randomGene = rand.nextInt(chromosomeLength);
     	chromosome[randomGene] = (rand.nextDouble() * geneRange) + geneOffset;
